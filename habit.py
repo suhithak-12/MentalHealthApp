356554f218f12c1570bb1caa0b64a665dbc7492e
Nancy Avila <45185192+avilan3@users.noreply.github.com>
2025-02-23 14:45:06 -0700

    Add files via upload

---
+++ habit.py
@@ -0,0 +1,17 @@
+import kivy.utils
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+from kivymd.uix.menu import MDDropdownMenu
+
+Window.size = (350, 600)
+
+class newHabit(MDApp):
+    def build(self):
+        return Builder.load_file('newHabit.kv')
+LabelBase.register(name='lemon', fn_regular='LEMONMILK-Regular.otf')
+LabelBase.register(name='kr', fn_regular='Krfontv3-Regular.ttf')
+
+if __name__ == '__main__':
+    newHabit().run()
