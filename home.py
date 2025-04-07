a8f6cdf87d3c6d49c58c91e4363f3a55fa4ff4cf
Jacky <jackelyn.hernandez@student.nmt.edu>
2025-04-07 11:29:35 -0600

    Happy button with random messages

--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 
 import kivy.utils
 import requests
+import random
 from kivymd.app import MDApp
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
@@ -19,6 +20,33 @@ class HomeScreen(Screen):  # Changed class name to avoid conflict
     def switch_screen(self, instance):
         self.dialog.dismiss()
         self.manager.current = "resources"
+
+    def show_happy_popup(self):
+        messages = [
+            "You're doing amazing! <3",
+            "Keep smiling, sunshine! :)",
+            "One step at a time, you're getting there!",
+            "You're stronger than you think <3",
+            "Take a deep breath and feel the peace :)",
+            "Today is full of possibilities!",
+            "Your effort matters. Always.",
+            "Go you!! <3",
+            "Penguin believes in you!",
+            "Sending good vibes your way :)"
+        ]
+        random_message = random.choice(messages)
+
+        self.dialog = MDDialog(
+            title="Happiness!",
+            text=random_message,
+            buttons=[
+                MDFlatButton(
+                    text="OK",
+                    on_release=lambda x: self.dialog.dismiss()
+                )
+            ]
+        )
+        self.dialog.open()   
 
 # Register the custom fonts
 LabelBase.register(name='lemon', fn_regular='LEMONMILK-Regular.otf')
