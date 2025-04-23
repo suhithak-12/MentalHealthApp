6d866b9b628280d77e595354eef572f15525b2b3
Nancy Avila <45185192+avilan3@users.noreply.github.com>
2025-04-23 15:15:20 -0600

    Update habit.py

--- habit.py
+++ habit.py
@@ -1,6 +1,8 @@
 import kivy.utils
+import mysql.connector
 import requests
 from kivymd.app import MDApp
+from kivy.app import App
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
 from kivy.lang import Builder
@@ -12,6 +14,29 @@ Window.size = (350, 600)
 
 class newHabit(Screen):  # Changed class name to avoid conflict
     pass
+    def createHabit(self):
+        username = App.get_running_app().username
+        habitName = self.ids.habitName.text
+        startDate = self.ids.startDate.text
+        dayFreq = self.ids.dayFreq.text
+        timeRemind = self.ids.timeRemind.text
+        Notes = self.ids.Notes.text
+
+        user_habit = {'username': username,'habitName' : habitName, 'startDate' : startDate,
+                    'dayFreq' : dayFreq, 'timeRemind' : timeRemind, 'Notes' : Notes}
+        url = "http://127.0.0.1:5000/newHabit"
+
+        try:
+            response = requests.post(url, data=user_habit)
+            response = response.json()
+            print(response)
+            print("New Habit Created!")
+            self.manager.current = "home"
+        except requests.exceptions.RequestException as e:
+            self.show_dialog(f"Network error: {str(e)}")
+        except ValueError:
+            self.show_dialog("Invalid server response")
+
     def switch_screen(self, instance):
         self.dialog.dismiss()
         self.manager.current = "home"
