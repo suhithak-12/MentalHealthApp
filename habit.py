0d07b28a59dc027a53e4ad16b28276ccfccb7891
Nancy Avila <45185192+avilan3@users.noreply.github.com>
2025-04-24 18:20:39 -0600

    Update habit.py

--- habit.py
+++ habit.py
@@ -1,6 +1,7 @@
 import kivy.utils
 import mysql.connector
 import requests
+from flask import session
 from kivymd.app import MDApp
 from kivy.app import App
 from kivymd.uix.dialog import MDDialog
@@ -9,20 +10,62 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.core.text import LabelBase
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivymd.uix.menu import MDDropdownMenu
+from functools import partial
 
 Window.size = (350, 600)
 
 class newHabit(Screen):  # Changed class name to avoid conflict
     pass
+    def dropdown1(self):
+        options = ["Daily", "Weekly", "Monthly", "Yearly"]
+        self.frequency_list = MDDropdownMenu(
+            caller = self.ids.frequency,
+            items = [
+                {
+                    "viewclass" : "OneLineListItem",
+                    "text" : i,
+                    "on_release": partial(self.select_option1, i)
+                } for i in options
+            ],
+            width_mult = 4
+        )
+        self.frequency_list.open()
+
+    def dropdown2(self):
+        options = ["AM", "PM"]
+        self.AmPm_list = MDDropdownMenu(
+            caller = self.ids.AmPm,
+            items = [
+                {
+                    "viewclass" : "OneLineListItem",
+                    "text" : i,
+                    "on_release": partial(self.select_option2, i)
+                } for i in options
+            ],
+            width_mult = 4
+        )
+        self.AmPm_list.open()
+
+    def select_option1(self, option):
+        self.ids.frequency.text = option
+        self.frequency_list.dismiss()
+        print(f"Selected: {option}")
+
+    def select_option2(self, option):
+        self.ids.AmPm.text = option
+        self.AmPm_list.dismiss()
+        print(f"Selected: {option}")
+
     def createHabit(self):
-        username = App.get_running_app().username
+        iduser = session.get('iduser')
         habitName = self.ids.habitName.text
         startDate = self.ids.startDate.text
         dayFreq = self.ids.dayFreq.text
         timeRemind = self.ids.timeRemind.text
         Notes = self.ids.Notes.text
 
-        user_habit = {'username': username,'habitName' : habitName, 'startDate' : startDate,
+        user_habit = {'iduser': iduser,'habitName' : habitName, 'startDate' : startDate,
                     'dayFreq' : dayFreq, 'timeRemind' : timeRemind, 'Notes' : Notes}
         url = "http://127.0.0.1:5000/newHabit"
 
